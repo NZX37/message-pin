92a48b66a268c6c33010278d917e5106c2946ef9
NZX <zx.ngzhengxian@gmail.com>
2025-06-18 07:43:26 +0800

    Update main.html

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-pip install PyGithub
-
-from github import Github
-
-g = Github("your_access_token")
-
-user = g.get_user()
-
-# Print all repositories of the authenticated user
-for repo in user.get_repos():
-print(repo.name)
-num = int(input("Number to check: "))
-flag = False
-if num == 0 or num == 1:
-    print(num, "is not a prime number")
-elif num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
-            flag = True
-            break
-    if flag:
-        print(num, "is not a prime number")
-    else:
-        print(num, "is a prime number")
+<!DOCTYPE html>
+<html>
+<head>
+<title>Prime Checker<title>
+<\head>
+<body>
+<h1>Prime Checker v.1<h1>
+<p>Welcome to Prime Checker v.1. Even though this has been done 2763 times, we are doing it again lol<h1>
+<\body>
+<\html>
