174eaefd7ecd52cddef26a3952d9e8d7a4ed23ff
NZX <zx.ngzhengxian@gmail.com>
2025-08-01 18:57:45 +0800

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,12 +1,12 @@
 // Replace with your Google Apps Script Web App URL
-const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwLGEPD8-fkSYqGgE0j4Ok4roqJWmemzxVP3Acm2BWQTd9i1xhLMQB6ufwSEKI6Wpys/exec';
+const BACKEND_URL = 'https://script.google.com/a/macros/students.edu.sg/s/AKfycbwLGEPD8-fkSYqGgE0j4Ok4roqJWmemzxVP3Acm2BWQTd9i1xhLMQB6ufwSEKI6Wpys/exec';
 
 document.getElementById('loginForm').onsubmit = async function(e) {
   e.preventDefault();
   const username = document.getElementById('loginUsername').value;
   const password = document.getElementById('loginPassword').value;
 
-  const res = await fetch(`${'https://script.google.com/macros/s/AKfycbwLGEPD8-fkSYqGgE0j4Ok4roqJWmemzxVP3Acm2BWQTd9i1xhLMQB6ufwSEKI6Wpys/exec'}?action=login`, {
+  const res = await fetch(`${'https://script.google.com/a/macros/students.edu.sg/s/AKfycbwLGEPD8-fkSYqGgE0j4Ok4roqJWmemzxVP3Acm2BWQTd9i1xhLMQB6ufwSEKI6Wpys/exec'}?action=login`, {
     method: 'POST',
     body: JSON.stringify({ username, password }),
     headers: { 'Content-Type': 'application/json' }
@@ -25,7 +25,7 @@ document.getElementById('signupForm').onsubmit = async function(e) {
   const username = document.getElementById('signupUsername').value;
   const password = document.getElementById('signupPassword').value;
 
-  const res = await fetch(`${'https://script.google.com/macros/s/AKfycbwLGEPD8-fkSYqGgE0j4Ok4roqJWmemzxVP3Acm2BWQTd9i1xhLMQB6ufwSEKI6Wpys/exec'}?action=signup`, {
+  const res = await fetch(`${'https://script.google.com/a/macros/students.edu.sg/s/AKfycbwLGEPD8-fkSYqGgE0j4Ok4roqJWmemzxVP3Acm2BWQTd9i1xhLMQB6ufwSEKI6Wpys/exec'}?action=signup`, {
     method: 'POST',
     body: JSON.stringify({ username, password }),
     headers: { 'Content-Type': 'application/json' }
